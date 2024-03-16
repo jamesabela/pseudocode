d4ce861a97982ec7129ad7537a028bf6774696ba
James Abela <abela.james@gmail.com>
2024-03-16 12:03:47 +0800

    Update operators.py
    Added DIV and MOD

--- operators.py
+++ operators.py
@@ -4,6 +4,9 @@
 *   	multiplication
 /   	division
 ^   	raised to the power of
+DIV(10, 3) returns 3
+MOD(10, 3) returns 1
+
 """
 
 # OUTPUT 5+5
@@ -20,3 +23,9 @@ print(30/5)
 
 # OUTPUT 2^4
 print(2**4)
+
+# OUTPUT DIV(10, 3)
+print(10//3)
+
+#OUTPUT MOD(10, 3)
+print(10%3)
