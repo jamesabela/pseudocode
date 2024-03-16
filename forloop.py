40cc6182bb5a650421d6a18a86ddc07cc0544f01
James Abela <abela.james@gmail.com>
2024-03-16 15:07:39 +0800

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,15 @@
+
+
+# FOR Counter ← 1 TO 10
+#     OUTPUT Counter
+# NEXT Counter
+
+for counter in range(1,11):
+    print(counter)
+
+# FOR Number ← 10 TO 1 STEP -1
+#     OUTPUT Number
+# NEXT Number
+
+for Number in range(10,0,-1):
+    print(Number)
