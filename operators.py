a0519ed732a95428f084322641592c3de7b3a8bc
James Abela <abela.james@gmail.com>
2024-03-16 11:58:57 +0800

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,22 @@
+"""""
++   	addition
+–   	subtraction
+*   	multiplication
+/   	division
+^   	raised to the power of
+"""
+
+# OUTPUT 5+5
+print(5+5)
+
+# OUTPUT 5-3
+print(5-3)
+
+# OUTPUT 5*5
+print(5*5)
+
+# OUTPUT 30/5
+print(30/5)
+
+# OUTPUT 2^4
+print(2**4)
