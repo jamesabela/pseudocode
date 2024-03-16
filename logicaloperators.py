6426018fef41470b057c11a104033f70e4762810
James Abela <abela.james@gmail.com>
2024-03-16 12:21:23 +0800

    Add files via upload

---
+++ logicaloperators.py
@@ -0,0 +1,26 @@
+"""
+Logical operators (8.1.4 (f))
+The following symbols are used for logical operators:
+= equal to
+<> not equal to
+< less than
+<= less than or equal to
+> greater than
+>= greater than or equal to
+
+The result of these operations is always of data type BOOLEAN.
+In complex expressions, it is advisable to use parentheses () to make the order of operations clear.
+
+"""
+# OUTPUT 5=3
+print(5==3) #False
+# OUTPUT 5<>3
+print(5!=3) #True
+# OUTPUT 5<3
+print(5<3) #False
+# OUTPUT 5>3
+print(5<=3) #False
+# OUTPUT 5>3
+print(5>3) #True
+# OUTPUT 5>=3
+print(5>=3) #True
